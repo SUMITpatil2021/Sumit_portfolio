e976a7c7a043f913f7946ce098f1914e38ce2990
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-07-03 23:55:13 +0530

    created downaladable link for downloading  resume

--- sumit_portfolio/src/components/landingPage.jsx
+++ sumit_portfolio/src/components/landingPage.jsx
@@ -2,9 +2,8 @@ import React, { useEffect,useRef,useState } from "react";
 import { Link } from "react-scroll";
 import { useNavigate, Routes} from "react-router-dom";
 import { Nav,Navbar,Container} from "react-bootstrap";
-
-
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faGraduationCap, faEnvelope, faBriefcase,faProjectDiagram ,faDownload } from '@fortawesome/free-solid-svg-icons';
 import Home from "./home";
 
 export default  function LandingPage(){
@@ -13,6 +12,26 @@ export default  function LandingPage(){
 
 const Navigate = useNavigate();
 const[selectedLink, setSelectedLink] = useState("home");
+
+const handleDownload = () => {
+  // Replace with your resume file URL or path
+  const resumeUrl = 'https://drive.google.com/file/d/1rBBANeaMXBHEAPCirV2S63T0Hogg67iK/view?usp=drive_link'; 
+
+  // Use JavaScript to create a link element
+  const link = document.createElement('a');
+  link.href = resumeUrl;
+  link.setAttribute('download', 'resume.pdf'); // Specify the file name here
+  link.setAttribute('target', '_blank'); //open in new tab 
+
+  // Append the link to the body
+  document.body.appendChild(link);
+
+  // Programmatically click the link to trigger the download
+  link.click();
+
+  // Clean up: Remove the link from the body
+  document.body.removeChild(link);
+};
 
 const handleLinkClick = (link)=>{
     setSelectedLink(link);
@@ -50,10 +69,24 @@ const renderComponent = ()=>{
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="ms-auto">
-                    <Nav.Link  onClick={()=>{handleLinkClick('home')}}>About Me</Nav.Link>
-                    {/* <Nav.Link onClick={()=>{handleLinkClick('')}} */}
-                    <Nav.Link  onClick={()=>{handleLinkClick('contactus')}}>Contact </Nav.Link>
-                   
+                    <Nav.Link  onClick={()=>{handleLinkClick('home')}}> 
+                      <FontAwesomeIcon icon={faUser}  /> 
+                      &nbsp;About Me</Nav.Link>
+
+                    <Nav.Link onClick={()=>{handleLinkClick('qualification')}}> 
+                      <FontAwesomeIcon icon={faGraduationCap}  />
+                      &nbsp;Qualification </Nav.Link>
+                    <Nav.Link onClick={()=>{handleLinkClick('workexp')}}>
+                    <FontAwesomeIcon icon={faBriefcase} />
+                    &nbsp;Work Experience </Nav.Link>
+
+                    <Nav.Link onClick={()=>{handleLinkClick('projects')}}>
+                    <FontAwesomeIcon icon={faProjectDiagram}  />
+                    &nbsp;Projects</Nav.Link>
+                    <Nav.Link  onClick={()=>{handleLinkClick('contactus')}}>
+                    <FontAwesomeIcon icon={faEnvelope} />
+                     &nbsp;Contact </Nav.Link>
+                   <Nav.Link onClick={()=>{handleDownload()}}> <FontAwesomeIcon icon={faDownload}/> &nbsp;Resume </Nav.Link>
                    
                  
                   </Nav>
