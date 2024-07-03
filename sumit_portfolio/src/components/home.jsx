d31bedb4369057b1c12498dd8859b89305b8637a
SUMITpatil2021 <sumit.patil2021@gmail.com>
2024-07-04 01:12:33 +0530

    changes in home component and make landing page stable

--- sumit_portfolio/src/components/home.jsx
+++ sumit_portfolio/src/components/home.jsx
@@ -1,38 +1,42 @@
 import React from "react";
 import { Link } from "react-scroll";
-
-import "./Home.css"; // Import a custom CSS file for any additional styling
-
-export default function Home() {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import './Home.css';
+const Home = () => {
     return (
-        <div className="container text-center my-5">
-            <div className="row justify-content-center">
+        <div className="about-us-container">
+            <div className="row align-items-center">
+                <div className="col-md-4 col-sm-6 col-xs-12 mb-3 mb-md-0">
+                    <img
+                        src="Sumit_ID_Photo.png"
+                        alt="Sumit ID Photo"
+                        className="id-photo img-fluid rounded-circle shadow border-2 border-primary"
+                        style={{ width: 'auto', height: 'auto' }}
+                    />
+                </div>
                 <div className="col-md-8">
-                    <div className="card shadow-sm">
-                        <img 
-                            src="sumit_ID_Photo.png" 
-                            alt="IDphoto" 
-                            className="card-img-top img-fluid rounded-circle mx-auto d-block mt-4" 
-                            style={{ width: '150px', height: '150px' }} // Fixed size for the image
-                        />
-                        <div className="card-body">
-                            <h5 className="card-title">Sumit</h5>
-                            <p className="card-text">
-                                Hello! I'm Sumit, an aspiring full-stack developer with a passion for creating dynamic and responsive web applications. I hold a CDAC certification, which has equipped me with a strong foundation in software development and the latest web technologies.
-                            </p>
-                            <p className="card-text">
-                                I am proficient in frontend and backend technologies including HTML, CSS, JavaScript, React, Node.js,Java SpringBoot,DOTNET Framework  and more. My goal is to continuously learn and improve my skills, and to contribute to innovative projects that make a difference.
-                            </p>
-                            <p className="card-text">
-                                Welcome to my portfolio. Feel free to explore my projects and get in touch with me.
-                            </p>
-                            <Link to="contact" spy={true} smooth={true}>
-                                <button className="btn btn-primary mt-3">Contact Me</button>
-                            </Link>
-                        </div>
+                    <div className="card-body">
+                        <h2 className="card-title">About Me</h2>
+                        <p className="card-text">
+                            Hello! I'm Sumit, an aspiring full-stack developer with a passion for creating dynamic and responsive web applications. I hold a CDAC certification, which has equipped me with a strong foundation in software development and the latest web technologies.
+                        </p>
+                        <p className="card-text">
+                            I am proficient in frontend and backend technologies including HTML, CSS, JavaScript, React, Node.js, Java SpringBoot, and more. My goal is to continuously learn and improve my skills, and to contribute to innovative projects that make a difference.
+                        </p>
+                        <p className="card-text">
+                            Welcome to my portfolio. Feel free to explore my projects and get in touch with me.
+                        </p>
+                        <button className="btn btn-primary">Get in Touch</button>
                     </div>
                 </div>
             </div>
         </div>
-    );
-}
+    )
+
+};
+
+export default Home;
+
+// CSS Styles
+
--- sumit_portfolio/src/components/landingPage.jsx
+++ sumit_portfolio/src/components/landingPage.jsx
@@ -1,104 +1,100 @@
-import React, { useEffect,useRef,useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-scroll";
-import { useNavigate, Routes} from "react-router-dom";
-import { Nav,Navbar,Container} from "react-bootstrap";
+import { useNavigate, Routes } from "react-router-dom";
+import { Nav, Navbar, Container } from "react-bootstrap";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser, faGraduationCap, faEnvelope, faBriefcase,faProjectDiagram ,faDownload } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faGraduationCap, faEnvelope, faBriefcase, faProjectDiagram, faDownload } from '@fortawesome/free-solid-svg-icons';
 import Home from "./home";
 
-export default  function LandingPage(){
+export default function LandingPage() {
 
 
 
-const Navigate = useNavigate();
-const[selectedLink, setSelectedLink] = useState("home");
+  const Navigate = useNavigate();
+  const [selectedLink, setSelectedLink] = useState("home");
 
-const handleDownload = () => {
-  // Replace with your resume file URL or path
-  const resumeUrl = 'https://drive.google.com/file/d/1rBBANeaMXBHEAPCirV2S63T0Hogg67iK/view?usp=drive_link'; 
+  const handleDownload = () => {
+    // Replace with your resume file URL or path
+    const resumeUrl = 'https://drive.google.com/file/d/1rBBANeaMXBHEAPCirV2S63T0Hogg67iK/view?usp=drive_link';
 
-  // Use JavaScript to create a link element
-  const link = document.createElement('a');
-  link.href = resumeUrl;
-  link.setAttribute('download', 'resume.pdf'); // Specify the file name here
-  link.setAttribute('target', '_blank'); //open in new tab 
+    // Use JavaScript to create a link element
+    const link = document.createElement('a');
+    link.href = resumeUrl;
+    link.setAttribute('download', 'resume.pdf'); // Specify the file name here
+    link.setAttribute('target', '_blank'); //open in new tab 
 
-  // Append the link to the body
-  document.body.appendChild(link);
+    // Append the link to the body
+    document.body.appendChild(link);
 
-  // Programmatically click the link to trigger the download
-  link.click();
+    // Programmatically click the link to trigger the download
+    link.click();
 
-  // Clean up: Remove the link from the body
-  document.body.removeChild(link);
-};
+    // Clean up: Remove the link from the body
+    document.body.removeChild(link);
+  };
 
-const handleLinkClick = (link)=>{
+  const handleLinkClick = (link) => {
     setSelectedLink(link);
-   // Navigate(`/${link}`)
-};
+    // Navigate(`/${link}`)
+  };
 
-useEffect(()=>{
+  useEffect(() => {
     setSelectedLink('home')
-},[]);
-const renderComponent = ()=>{
-    switch(selectedLink){
+  }, []);
+  const renderComponent = () => {
+    switch (selectedLink) {
       case "home":
-            return <Home/>
-        // case "aboutus": 
-        //   return <AboutUs/>
-        // case "contactus":
-        //     return <ContactUs/>
-        // case "signin":
-        //   return <UserLogin/>
-        default:
-          return null;
+        return <Home />
+      // case "aboutus": 
+      //   return <AboutUs/>
+      // case "contactus":
+      //     return <ContactUs/>
+      // case "signin":
+      //   return <UserLogin/>
+      default:
+        return null;
     }
-}
+  }
 
-    return (
-       <>
-   
+  return (
+    <> 
+      <div> 
+        <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+          <Container>
+            <Navbar.Brand href="/">SUMIT PATIL </Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                <Nav.Link onClick={() => { handleLinkClick('home') }}>
+                  <FontAwesomeIcon icon={faUser} />
+                  &nbsp;About Me</Nav.Link>
 
-     
-       
-      <div>
-      <Navbar bg="dark" variant="dark" expand="lg">
-              <Container>  
-                <Navbar.Brand href="/">SUMIT PATIL </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                  <Nav className="ms-auto">
-                    <Nav.Link  onClick={()=>{handleLinkClick('home')}}> 
-                      <FontAwesomeIcon icon={faUser}  /> 
-                      &nbsp;About Me</Nav.Link>
+                <Nav.Link onClick={() => { handleLinkClick('qualification') }}>
+                  <FontAwesomeIcon icon={faGraduationCap} />
+                  &nbsp;Qualification </Nav.Link>
+                <Nav.Link onClick={() => { handleLinkClick('workexp') }}>
+                  <FontAwesomeIcon icon={faBriefcase} />
+                  &nbsp;Work Experience </Nav.Link>
 
-                    <Nav.Link onClick={()=>{handleLinkClick('qualification')}}> 
-                      <FontAwesomeIcon icon={faGraduationCap}  />
-                      &nbsp;Qualification </Nav.Link>
-                    <Nav.Link onClick={()=>{handleLinkClick('workexp')}}>
-                    <FontAwesomeIcon icon={faBriefcase} />
-                    &nbsp;Work Experience </Nav.Link>
+                <Nav.Link onClick={() => { handleLinkClick('projects') }}>
+                  <FontAwesomeIcon icon={faProjectDiagram} />
+                  &nbsp;Projects</Nav.Link>
+                <Nav.Link onClick={() => { handleLinkClick('contactus') }}>
+                  <FontAwesomeIcon icon={faEnvelope} />
+                  &nbsp;Get In Touch  </Nav.Link>
+                <Nav.Link onClick={() => { handleDownload() }}> <FontAwesomeIcon icon={faDownload} /> &nbsp;Resume </Nav.Link>
 
-                    <Nav.Link onClick={()=>{handleLinkClick('projects')}}>
-                    <FontAwesomeIcon icon={faProjectDiagram}  />
-                    &nbsp;Projects</Nav.Link>
-                    <Nav.Link  onClick={()=>{handleLinkClick('contactus')}}>
-                    <FontAwesomeIcon icon={faEnvelope} />
-                     &nbsp;Contact </Nav.Link>
-                   <Nav.Link onClick={()=>{handleDownload()}}> <FontAwesomeIcon icon={faDownload}/> &nbsp;Resume </Nav.Link>
-                   
-                 
-                  </Nav>
-                </Navbar.Collapse>
-              </Container>
-            </Navbar>
+
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
       </div>
 
       <div>
-      {renderComponent()}
+        {renderComponent()}
       </div>
 
-      </>
-    )
+    </>
+  )
 }
